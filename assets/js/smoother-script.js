7ebf253c358cf8161180d699afe533fdf45c41e0
arpan <me.arpan122@gmail.com>
2024-07-18 19:11:56 +0530

    first commit

---
+++ assets/js/smoother-script.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+    ScrollTrigger.normalizeScroll(true)
+
+    // create the smooth scroller FIRST!
+    let smoother = ScrollSmoother.create({
+        smooth: 2,
+        effects: true,
+    });
+
+});
